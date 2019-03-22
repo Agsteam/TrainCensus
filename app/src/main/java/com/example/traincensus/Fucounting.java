7ec07bd03c8b50e5f7323ec673ea5d4a01357e52
Admin <agsteamcensus@gmail>
2019-03-22 17:43:09 +0530

    recycler view init 22/3

--- app/src/main/java/com/example/traincensus/Fucounting.java
+++ app/src/main/java/com/example/traincensus/Fucounting.java
@@ -15,7 +15,10 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.traincensus.Model.TrainDetailsResponse;
@@ -38,7 +41,11 @@ import okhttp3.ResponseBody;
 
 public class Fucounting extends AppCompatActivity
 {
+    int nugs,nugsrd,nugslrd,nugs1,nugslr,nugsld;
     String c,code="";
+    CheckBox g1,g2,g3,g4,g5,g6;
+    EditText g1c,g2c,g3c,g4c,g5c,g6c;
+    TextView total;
     AutoCompleteTextView listView;
     List<String> list;
     Button trains1;
@@ -52,8 +59,34 @@ public class Fucounting extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fucounting);
         trains1=(Button)findViewById(R.id.button);
+        g1=(CheckBox)findViewById(R.id.gsr);
+        g2=(CheckBox)findViewById(R.id.gsrd);
+        g3=(CheckBox)findViewById(R.id.gslrd);
+        g4=(CheckBox)findViewById(R.id.gs);
+        g5=(CheckBox)findViewById(R.id.gslr);
+        g6=(CheckBox)findViewById(R.id.wgscz);
+        g1c=(EditText)findViewById(R.id.gsrv);
+        g2c=(EditText)findViewById(R.id.gsrdv);
+        g3c=(EditText)findViewById(R.id.gslrdv);
+        g4c=(EditText)findViewById(R.id.gsv);
+        g5c=(EditText)findViewById(R.id.gslrv);
+        g6c=(EditText)findViewById(R.id.wgsczv);
+        total=(TextView)findViewById(R.id.tc);
         listView = (AutoCompleteTextView) findViewById(R.id.tno);
         super.onStart();
+        total.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(g1.isChecked()==true)
+                    if(!g1c.getText().toString().equals(""))
+                    {
+                            nugs = Integer.parseInt(g1c.getText().toString());
+                            Log.e("sun", "sun" + Integer.parseInt(g1c.getText().toString()));
+
+                    }
+            }
+        });
+
         for(int i=0;i<Welcomescreen.sta.length();i++)
         {
             int za=0;
@@ -78,7 +111,6 @@ public class Fucounting extends AppCompatActivity
     }
     public void sun1()
     {
-
         client.newCall(request).enqueue(new Callback()
         {
             @Override public void onFailure(Call call, IOException e)
@@ -97,12 +129,11 @@ public class Fucounting extends AppCompatActivity
                     {
                         String trainname = trainDetailsResponse.getTrains().get(i).getNumber()+"\t"+trainDetailsResponse.getTrains().get(i).getName();
                         list.add(trainname);
-                        Log.e("sun1",list.get(i));
                     }
                     Fucounting.this.runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
-                            adapter = new ArrayAdapter<String>(Fucounting.this, R.layout.auto_complete_item, list);
+                            adapter = new ArrayAdapter<String>(Fucounting.this, android.R.layout.simple_list_item_1, list);
                             listView.setAdapter(adapter);
                             adapter.notifyDataSetChanged();
                         }
