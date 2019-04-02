133e175de21c83222bf8dd4be1ffbd4778d34626
sundarselvi <sundar.k.selvi3@gmail.com>
2019-04-02 14:56:15 +0530

    report done

--- app/src/main/java/com/example/traincensus/Report.java
+++ app/src/main/java/com/example/traincensus/Report.java
@@ -13,40 +13,100 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Report extends AppCompatActivity {
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference ref = database.getReference("field");
     EditText et;
     Button bt;
-    String st[]={""},dt[]={""};
-    int i=0,tav[]={0},j;
+    ArrayList<String> st = new ArrayList<String>();
+    ArrayList<String> dt = new ArrayList<String>();
+    ArrayList<Integer> tav = new ArrayList<Integer>();
+    ArrayList<Integer> j = new ArrayList<Integer>();
+    int i,t,i1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
         et=findViewById(R.id.editText2);
         bt=findViewById(R.id.button3);
-        bt.setOnClickListener(new View.OnClickListener() {
+        st.add("");
+        dt.add("");
+        bt.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View v) {
-                ref.addValueEventListener(new ValueEventListener() {
+            public void onClick(View v)
+            {
+                i=0;i1=0;t=0;
+                ref.addValueEventListener(new ValueEventListener()
+                {
                     @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-                        for (DataSnapshot postSnapshot: dataSnapshot.getChildren())
+                    public void onDataChange(DataSnapshot dataSnapshot)
+                    {
+                        for (DataSnapshot postSnapshot1: dataSnapshot.getChildren())
                         {
-                            Adddata1 post = postSnapshot.getValue(Adddata1.class);
-                            String tq=post.getTrain();
-                            tq=tq.substring(0,5);
-                            if ((st[i].equals("") || st[i].equals(post.getStation())) && (dt[i].equals("") || (dt[i].equals(post.getDudate())))&&(Integer.parseInt(et.getText().toString())==Integer.parseInt(tq)))
+                            Adddata1 post1 = postSnapshot1.getValue(Adddata1.class);
+                            String tq1 = post1.getTrain();
+                            tq1 = tq1.substring(0, 5);
+                            st.add(i,post1.getStation());
+                            dt.add(i,post1.getDudate());
+                            tav.add(i1,0);
+                            j.add(i1,0);
+                            if (st.get(i).equals(post1.getStation()) && (dt.get(i).equals(post1.getDudate())) && (Integer.parseInt(et.getText().toString()) == Integer.parseInt(tq1)))
                             {
-                                        st[i] = post.getStation();
-                                        tav[i] += post.getAv();
-                                        dt[i] = post.getDudate();
-                                        Log.e("avg", "" + tav[i]);
+                                for(int k=0;k<=i;k++)
+                                if((st.get(k).equals(st.get(t)))&&(t!=1))
+                                {
+                                         st.add(i,post1.getStation());
+                                         dt.add(i,post1.getDudate());
+                                        for (DataSnapshot postSnapshot : dataSnapshot.getChildren())
+                                        {
+                                            Adddata1 post = postSnapshot.getValue(Adddata1.class);
+                                            String tq = post.getTrain();
+                                            tq = tq.substring(0, 5);
+                                            if ((st.get(i).equals(post.getStation())) &&  (dt.get(i).equals(post.getDudate())) && (Integer.parseInt(et.getText().toString()) == Integer.parseInt(tq)))
+                                            {
+                                                int temp=tav.get(i1);
+                                                temp+=post.getAv();
+                                                tav.add(i1,temp);
+                                                j.add(i1,j.get(i1)+1);
+                                            }
+                                        }
+                                        t=1;
+                                    i++;i1++;
+                                }
+                                else if(!(st.get(k).equals(post1.getStation())) && (dt.get(i).equals("") || (dt.get(i).equals(post1.getDudate()))) && (Integer.parseInt(et.getText().toString()) == Integer.parseInt(tq1)))
+                                {
+                                    st.add(i,post1.getStation());
+                                    dt.add(i,post1.getDudate());
+                                    for (DataSnapshot postSnapshot : dataSnapshot.getChildren())
+                                    {
+                                        Adddata1 post = postSnapshot.getValue(Adddata1.class);
+                                        String tq = post.getTrain();
+                                        tq = tq.substring(0, 5);
+                                        if ((st.get(i).equals(post.getStation())) &&  (dt.get(i).equals(post.getDudate())) && (Integer.parseInt(et.getText().toString()) == Integer.parseInt(tq)))
+                                        {
+                                            int temp=tav.get(i1)+post.getAv();
+                                            tav.add(i1,temp);
+                                            j.add(i1,j.get(i1)+1);
+                                        }
+                                    }
+                                    i++;i1++;
+                                }
                             }
                         }
+                        for(int h=0;h<i;h++)
+                        {
+                            Log.e("train no",et.getText().toString());
+                            Log.e("Station",st.get(h));
+                            Log.e("Date",dt.get(h));
+                            Log.e("Total",""+tav.get(h));
+                            if(i!=0&&j.get(h)!=0 )
+                                Log.e("Percentage",""+(tav.get(h)/j.get(h)));
+                        }
                     }
-
                     @Override
                     public void onCancelled(DatabaseError databaseError) {
                         Log.e("The read failed: " , ""+databaseError.getCode());
